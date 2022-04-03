f3816f47b87756ba521acfeb8c848d9dbbf4111a
Fernando Silva <fernandoadmti@hotmail.com>
2022-04-03 18:23:00 -0300

    install mongoose

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 'use strict'
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//string conection
+mongoose.connect('mongodb://localhost:27017/');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
